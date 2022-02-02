644bf86888eb96289e4d008a802a882568364233
Ava <avamsteimle@gmail.com>
2022-02-02 00:01:52 -0800

    fixed foreign key syntac

--- db/schema.sql
+++ db/schema.sql
@@ -3,24 +3,25 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE department (
+CREATE TABLE department(
     id INT NOT NULL PRIMARY KEY,
     name VARCHAR(30)
 );
 
 CREATE TABLE roles (
-    id INT PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
     dpt_id INT,
-    FOREIGN KEY (dpt_id)
-    REFERENCES department(id)
+    FOREIGN KEY(dpt_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
     id INT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
-    role_id INT FOREIGN KEY REFERENCES roles(id),
-    manager_id INT  FOREIGN KEY REFERENCES employee(id)
+    role_id INT,
+    manager_id INT,
+    FOREIGN KEY (role_id) REFERENCES roles(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id)
 );
