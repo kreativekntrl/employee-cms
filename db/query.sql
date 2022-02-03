0a32997c6f5d4f607c578af1502c41ee7e05dd75
Ava <avamsteimle@gmail.com>
2022-02-02 20:35:11 -0800

    added second join

--- db/query.sql
+++ db/query.sql
@@ -3,6 +3,11 @@
 -- JOIN <right_table>
 -- ON <left_table>.<column> = <right_table>.<column>;
 
-SELECT roles.id, title, salary, dpt_name
+SELECT employee.id, first_name, last_name, title, salary, dpt_name
 FROM department
-JOIN roles ON department.id = roles.dpt_id;
+JOIN roles ON department.id = roles.dpt_id
+JOIN employee ON employee.role_id = roles.id;
+
+--SELECT employee.id, first_name, last_name, title, salary, 
+--FROM roles
+--JOIN employee ON roles.id = employee.role_id;
