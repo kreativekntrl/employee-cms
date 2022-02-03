4b5dacdeecdd8d808d3ce5486c23b8a6458f6ee4
Ava <avamsteimle@gmail.com>
2022-02-02 17:35:15 -0800

    roles table seeded

--- db/schema.sql
+++ db/schema.sql
@@ -9,15 +9,15 @@ CREATE TABLE department(
 );
 
 CREATE TABLE roles (
-    id INT NOT NULL PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30),
     salary DECIMAL,
-    dpt_id INT,
+    dpt_id INT NOT NULL,
     FOREIGN KEY(dpt_id) REFERENCES department(id)
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30),
     last_name VARCHAR(30),
     role_id INT,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,16 @@
 INSERT INTO department(dpt_name)
-VALUES ("Engineering"),
-       ("Finance"),
-       ("Legal"),
-       ("Sales");
+VALUES("Engineering"),
+      ("Finance"),
+      ("Legal"),
+      ("Sales");
+  
+
+INSERT INTO roles(title, salary, dpt_id)
+VALUES("Sales Lead", 100.0, 1),
+      ("Salesperson", 80.0, 2),
+      ("Lead Engineer", 150.0, 3),
+      ("Software Engineer", 120.0, 4),
+      ("Account Manager", 160.0, 5),
+      ("Accountant", 125.0, 6),
+      ("Legal Team Lead", 250.0, 7),
+      ("Lawyer", 190.0, 8);
