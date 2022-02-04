5c3846de69fdc2ac29da97ed45eebad2691b4b53
Ava <avamsteimle@gmail.com>
2022-02-03 18:38:33 -0800

    switch case calls functions for respective user choices

--- server.js
+++ server.js
@@ -3,6 +3,12 @@ const inquirer = require("inquirer");
 const {
     listen
 } = require("express/lib/application");
+const {
+    response
+} = require("express");
+const {
+    registerPrompt
+} = require("inquirer");
 
 // creates the connection server.js and mySQL
 const db = mysql.createConnection({
@@ -12,16 +18,17 @@ const db = mysql.createConnection({
     database: 'employee_db'
 });
 
+//MAIN MENU PROMPT 
 db.query(
     "SELECT employee.id, first_name, last_name, title, salary, dpt_name FROM department JOIN roles ON department.id = roles.dpt_id JOIN employee ON employee.role_id = roles.id;",
-    async function (err, allData) {
+    async function (err) {
         if (err) {
             console.error(err);
             process.exit(1);
         }
-        const {
+        const 
             menuOption
-        } = await inquirer.prompt([{
+         = await inquirer.prompt([{
             type: "list",
             message: "What would you like to do?",
             name: "mainMenu",
@@ -34,7 +41,34 @@ db.query(
                 "View all Employees",
                 "Add employee",
                 "QUIT"
-            ]
+            ],
         }])
+        switch (menuOption.mainMenu) {
+            case "View all Roles":
+                console.log("hello");
+                break;
+            case "Update employee role":
+                updateRole();
+                break;
+            case "Add Role":
+                addRole();
+                break;
+            case "View all departments":
+                viewDepartments();
+                break;
+            case "Add Department":
+                addDepartment();
+                break;
+            case "View all Employees":
+                viewEmployees();
+                break;
+            case "Add employee":
+                addEmployee();
+                break;
+            case "QUIT":
+                db.end();
+            default:
+                db.end();
+        }
     }
-)
+    );
